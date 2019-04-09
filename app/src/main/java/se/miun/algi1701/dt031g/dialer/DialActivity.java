2290e5c6423403e890dbff4f9128ed1d6d421bbe
Alexander Gillberg <algi1701@student.miun.se>
2019-04-09 10:46:02 +0200

    Assignment 6 started.

--- app/src/main/java/se/miun/algi1701/dt031g/dialer/DialActivity.java
+++ app/src/main/java/se/miun/algi1701/dt031g/dialer/DialActivity.java
@@ -1,14 +1,17 @@
 package se.miun.algi1701.dt031g.dialer;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -18,6 +21,7 @@ import java.io.File;
 public class DialActivity extends AppCompatActivity {
 
     private final int PERMISSION_REQUEST_READ_EXTERNAL_STORAGE = 1;
+    private final int PERMISSION_REQUEST_PHONE_CALL = 2;
 
     private Dialpad dialpad;
 
@@ -34,12 +38,24 @@ public class DialActivity extends AppCompatActivity {
         dialButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:" + Uri.encode(dialpad.getNumber())));
-                startActivity(intent);
+
+                if(ContextCompat.checkSelfPermission(DialActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(DialActivity.this, new String[]{Manifest.permission.CALL_PHONE}, PERMISSION_REQUEST_PHONE_CALL);
+                }
+                else{
+                    makeCall();
+                }
+
             }
         });
 
+
+    }
+
+    private void makeCall(){
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + Uri.encode(dialpad.getNumber())));
+        startActivity(intent);
 
     }
 
@@ -53,6 +69,8 @@ public class DialActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+
 
         switch (requestCode){
             case PERMISSION_REQUEST_READ_EXTERNAL_STORAGE:{
@@ -73,7 +91,28 @@ public class DialActivity extends AppCompatActivity {
                     Toast.makeText(this, "Permission denied to read the external storage", Toast.LENGTH_SHORT).show();
                 }
 
+                break;
+
             }
+
+
+            case PERMISSION_REQUEST_PHONE_CALL:{
+
+
+
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //Permission granted!
+                    makeCall();
+                }
+                else{
+                    // Permission denied!
+                    Toast.makeText(this, "Permission denied to make call from phone", Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+
+            }
+
         }
 
     }
