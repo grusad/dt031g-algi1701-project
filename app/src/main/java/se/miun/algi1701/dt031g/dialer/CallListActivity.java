a8eb4ef3d5fbd7232716ff17d3f0a3ea30309e64
Alexander Gillberg <algi1701@student.miun.se>
2019-04-17 13:58:56 +0200

    fixed last task in assigntment 5

--- app/src/main/java/se/miun/algi1701/dt031g/dialer/CallListActivity.java
+++ app/src/main/java/se/miun/algi1701/dt031g/dialer/CallListActivity.java
@@ -1,7 +1,13 @@
 package se.miun.algi1701.dt031g.dialer;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Set;
 
 public class CallListActivity extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class CallListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_list);
+
+        loadList();
+    }
+
+    private void loadList(){
+        TextView textView = (TextView) findViewById(R.id.numbers);
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        Set<String> numbers = sharedPref.getStringSet("numbers", null);
+
+        if (numbers == null){
+            textView.setText("No numbers are stored.");
+        }
+
+        else{
+            for(String number : numbers){
+                textView.append(number + "\n");
+            }
+        }
     }
 }
--- app/src/main/java/se/miun/algi1701/dt031g/dialer/DialActivity.java
+++ app/src/main/java/se/miun/algi1701/dt031g/dialer/DialActivity.java
@@ -1,22 +1,25 @@
 package se.miun.algi1701.dt031g.dialer;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
-
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DialActivity extends AppCompatActivity {
 
@@ -31,6 +34,10 @@ public class DialActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         dialpad = new Dialpad(this);
         setContentView(dialpad);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_READ_EXTERNAL_STORAGE);
 
         ImageButton dialButton = (ImageButton) findViewById(R.id.dial_button);
@@ -52,11 +59,49 @@ public class DialActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.settings_item: {
+                startActivity(new Intent(DialActivity.this, SettingsActivity.class));
+                break;
+            }
+        }
+        return true;
+    }
+
     private void makeCall(){
         Intent intent = new Intent(Intent.ACTION_CALL);
         intent.setData(Uri.parse("tel:" + Uri.encode(dialpad.getNumber())));
+
+        if(PreferenceManager.getDefaultSharedPreferences(this).getBoolean("store_numbers", true)){
+           storeNumber();
+        }
+
         startActivity(intent);
 
+    }
+
+    private void storeNumber(){
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        Set<String> numbers = sharedPref.getStringSet("numbers", null);
+        if(numbers == null){
+            numbers = new HashSet<>();
+        }
+
+        numbers.add(dialpad.getNumber());
+        editor.putStringSet("numbers", numbers);
+        editor.apply();
+        editor.commit();
     }
 
     @Override
